7d5725876aebf40ed4bc6a8b5bf54d3109b7e664
antonio-vigilante <antoniovigilante@autistici.org>
2025-09-30 09:43:18 +0200

    nome

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "./trame",
+    pageTitle: "trame",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
