219b6311f03f4db5890fbae43db81f5f20253a91
antonio-vigilante <antoniovigilante@autistici.org>
2025-10-01 16:04:29 +0200

    img

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "it-IT",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
