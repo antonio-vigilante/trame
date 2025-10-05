f31130601035848684d59eda432b0c768870abc5
antonio-vigilante <antoniovigilante@autistici.org>
2025-10-05 10:47:47 +0200

    Colori

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,13 +23,13 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
+        header: "EB Garamond",
         body: "Noto Sans",
         code: "IBM Plex Mono",
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          light: "#ffffff",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
