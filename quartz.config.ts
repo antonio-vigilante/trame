ca5c3846babb53ae5ce7c653eb1245434f7b45b8
antonio-vigilante <antoniovigilante@autistici.org>
2025-11-30 21:41:00 +0100

    Url

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "it-IT",
-    baseUrl: "trame.pages.dev",
+    baseUrl: "https://trame.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
