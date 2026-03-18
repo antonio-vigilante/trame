026eba4f1a0b54d6c0d5a4983659acd2f0aace67
Antonio <antoniovigilante@autistici.org>
2026-03-18 15:29:55 +0100

    Font

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Schibsted Grotesk",
-        body: "Georgia",
+        body: "IBM Plex Mono",
         code: "IBM Plex Mono",
       },
       colors: {
